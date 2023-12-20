36ea3d03c6e759a58045d5055b32d3daa9f41b5b
Tamar Reuveny <Tamar.reuveny@runi.ac.il>
2023-12-20 18:15:45 +0200

    edit

--- GenThree.java
+++ GenThree.java
@@ -4,6 +4,30 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		// Put your code here	
+
+		int a = Integer.parseInt(args[0]);
+
+	    int b = Integer.parseInt(args[1]);
+
+	    int max = Math.max(a,b);
+
+	    int min = Math.min(a,b);
+
+	    int minus = max - min;
+
+	    int minimal = max;
+
+	    for (int i = 0 ; i < 3 ; i ++) {
+
+	    int random = (int)((Math.random()) * (minus)) + min;
+
+	    System.out.println(random);
+
+	    while (random < minimal) {
+
+	    minimal = random;
+	    }
+	    }
+	    System.out.println("The minimal generated number was " + minimal);	
 	}
 }
