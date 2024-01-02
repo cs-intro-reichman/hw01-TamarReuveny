003309962da1200d60401f307122fb2454f4726b
runiAviv <155253539+runiAviv@users.noreply.github.com>
2024-01-02 21:21:36 +0200

    Update GenThree.java

--- GenThree.java
+++ GenThree.java
@@ -4,30 +4,27 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-
 		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		
+		int max = Math.max(a,b);
+		int min = Math.min(a,b);
+		
+		int minus = max - min;
+		int minimal = max;
+		
+		for (int i = 0 ; i < 3 ; i ++) {
+			int random = (int)((Math.random()) * (minus)) + min;
+			System.out.println(random);
 
-	    int b = Integer.parseInt(args[1]);
-
-	    int max = Math.max(a,b);
-
-	    int min = Math.min(a,b);
-
-	    int minus = max - min;
-
-	    int minimal = max;
-
-	    for (int i = 0 ; i < 3 ; i ++) {
-
-	    int random = (int)((Math.random()) * (minus)) + min;
-
-	    System.out.println(random);
-
-	    while (random < minimal) {
-
-	    minimal = random;
-	    }
-	    }
-	    System.out.println("The minimal generated number was " + minimal);	
+			// #feedback: why do you need this loop? - it will itterate only once anyway since at the next itteration random = minimal
+			// you might use if instead or math.min
+			// if (random < minimal) {
+			// 	minimal = random;
+			// }
+			minmal = Math.Min(minimal, random);
+		}
+		
+		System.out.println("The minimal generated number was " + minimal);	
 	}
 }
